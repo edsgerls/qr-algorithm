0331aa43e05fa63e64d87784a08fb64d098e73db
edsgerls <edgar.lopez06@outlook.com>
2022-01-27 21:45:35 -0600

    Adding initial implementation for symmetric matrices (to compute real eigenvalues)

--- src/gram_schmidt.py
+++ src/gram_schmidt.py
@@ -3,31 +3,20 @@ import numpy as np
 # TODO Comment every section
 
 def gram_schmidt_ortonorm(A):
-
     m, n = np.shape(A)
     tmp = []
 
     for k in range(n):
-
         vk = A[:,k]
-        print(vk)
-
         Q = np.transpose(np.array(tmp))
-        print('Printing Q')
-        print(Q)
-
 
         proy = 0
         for i in range(1, k+1):
-            print('Imprimir la proyeccion')
             ui = Q[:,i-1]
             proy += (np.dot(vk,ui)) * ui
-            print(proy)
 
         vpk = vk - proy
-
         ui = vpk / np.linalg.norm(vpk)
-
         tmp.append(ui)
 
     return np.transpose(np.array(tmp))
--- src/main.py
+++ src/main.py
@@ -6,9 +6,22 @@ import gram_schmidt as ga
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
 
-    print('Hola')
-    A = np.array([[1,0,1],[1,1,0],[0,1,1]], np.int32)
-    Q = ga.gram_schmidt_ortonorm(A)
+    A = np.array([[2,1],[1,2]])
 
+    P = np.identity(2)
 
+    while not (np.allclose(A,np.tril(A)) or np.allclose(A,np.triu(A))):
+
+        Q = ga.gram_schmidt_ortonorm(A)
+        P = np.matmul(P,Q)
+
+        # Compute Ri matrix
+        R = np.matmul(np.transpose(Q),A)
+
+        Anew = np.matmul(R,Q)
+
+        A = Anew
+
+    print(A.diagonal())
+    print(P)
 
